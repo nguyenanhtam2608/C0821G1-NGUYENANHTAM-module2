ffb902d295cc6df832b0ad06fa6279bcfef267d7
nguyenanhtam2608 <nguyenanhtam12344321@gmail.com>
2021-10-02 15:10:08 +0700

    nat

---
+++ 1. Introduction to Java/thực hành/hienthithoigiancuahethong.java
@@ -0,0 +1,8 @@
+import java.util.Date;
+
+public class hienthithoigiancuahethong {
+    public static void main(String[] args) {
+        Date time = new Date();
+        System.out.println(time);
+    }
+}
