218ffbfc4278cb2c2d65dee924f4afad679b6378
nguyenanhtam2608 <nguyenanhtam12344321@gmail.com>
2021-10-07 22:14:42 +0700

    Fan

--- bai_4_OOP/Baitap/Fan.java
+++ bai_4_OOP/Baitap/Fan.java
@@ -1,28 +1,18 @@
 package bai_4_OOP.Baitap;
 
 
-import java.util.Scanner;
-
 public class Fan {
-    final int SLOW = 1;
-    final int MEDIUM = 2;
-    final int FAST = 3;
-    int speed = SLOW;
-    boolean on = true;
-    double radius = 5;
+    public static final int SLOW = 1; // khai báo hằng cần phải có public static
+    public static final int MEDIUM = 2;
+    public static final int FAST = 3;
+    private int speed = SLOW;// private tính bao đóng và ẩn dữ liệu bên ngoài vì các biến sau private chỉ truy cấ[j bên trong lớp;
+    private boolean on = false;
+    private double radius = 5;
     String color = "blue";
 
-    public int getSLOW() {
-        return SLOW;
+    public Fan() {// khởi tạo không tham số Contructor
     }
 
-    public int getMEDIUM() {
-        return MEDIUM;
-    }
-
-    public int getFAST() {
-        return FAST;
-    }
 
     public int getSpeed() {
         return speed;
@@ -56,27 +46,36 @@ public class Fan {
         this.color = color;
     }
 
-    public Fan() {// khởi tạo đối tượng không tham số
-    }
 
     public String toString() {
-        if (this.on == true) {
-            System.out.println("Tốc độ " + speed);
-            System.out.println("Color " + color);
-            System.out.println("Radius " + radius);
-            System.out.println("Fan is On ");
+        String status = "";
+        if (this.on) {
+            status = "Tốc độ " + speed + " Color " + color + " Radius " + radius + " Fan is On";
+
         } else {
-            System.out.println("Color : " + color);
-            System.out.println("Radius : " + radius);
-            System.out.println("Fan is off ");
+            status = "Color " + color + " Radius " + radius + " Fan is Off ";
         }
-        return " ";
+        return status;
     }
+
+
     public static void main(String[] args) {
         System.out.println("Các thông tin của Fan :");
+        System.out.println("Fan1");
+        Fan fan1 = new Fan();//gọi đối tượng thông qua từ khóa new (khoiwe tạo đối tượng thông qa từ khóa new)
+        fan1.setSpeed(Fan.FAST);
+        fan1.setRadius(10);
+        fan1.setColor("yellow");
+        fan1.setOn(true);
+        System.out.println(fan1);
+        System.out.println("Fan2");
+        Fan fan2 = new Fan();
+        fan2.setSpeed(MEDIUM);
+        fan2.setRadius(5);
+        fan2.setColor("Blue");
+        fan2.setOn(false);
+        System.out.println(fan2);
 
-        Fan fan = new Fan();
-        fan.toString();
 
     }
 }
